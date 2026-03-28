ab3364023205eef5632e9efe881b49e75b639d43
ariel.jorge <to.ariel.jorge+github@gmail.com>
2026-03-28 09:58:34 -0300

    start: iniciando projeto

---
+++ sgto-backend/src/main/java/com/github/arieljorge/sgto/SgtoApplication.java
@@ -0,0 +1,13 @@
+package com.github.arieljorge.sgto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgtoApplication.class, args);
+    }
+
+}
---
+++ sgto-backend/src/test/java/com/github/arieljorge/sgto/SgtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.arieljorge.sgto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
